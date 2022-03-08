c0a141ed32975485265aa3b1a51165cc44bee387
luffy <michel.estrada13@gmail.com>
2022-03-08 12:42:07 -0600

    Se termina el codigo

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from enum import Enum
 
 # Pydantic
 from pydantic import BaseModel
-from pydantic import Field
+from pydantic import Field, EmailStr
 
 # FastAPI
-from fastapi import FastAPI, Path, Query, Body
+from fastapi import Cookie, File, Form, Header, Path, Query, Body, UploadFile 
+from fastapi import FastAPI 
 from fastapi import status
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -20,6 +22,14 @@ class HairColor(Enum):
     black = "black"
     blonde = "blonde"
     red = "red"
+
+class LoginOut(BaseModel):
+    username: str = Field(
+        ...,
+        max_length=20,
+        example = "totoro"
+    )
+    message: str = Field(default="Login Succesfully")
 
 class Location(BaseModel):
     city: str
@@ -57,7 +67,9 @@ class PersonOut(PersonBase):
 
 @app.get(
     path="/",
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    #deprecated= True, 
+    tags=["Home"]
     )
 def home():
     return {"Hello":"World"}
@@ -67,7 +79,8 @@ def home():
 @app.post(
     path="/person/new", 
     response_model = PersonOut,
-    status_code=status.HTTP_201_CREATED)
+    status_code=status.HTTP_201_CREATED,
+    tags=["Persons"])
                                    # Obliga a que el parametro person en el request body sea obligatorio
 def create_person(person: Person = Body(...)):
     return person
@@ -76,7 +89,8 @@ def create_person(person: Person = Body(...)):
 
 @app.get(
     path="/person/detail",
-    status_code=status.HTTP_200_OK)
+    status_code=status.HTTP_200_OK,
+    tags=["Persons"])
 def show_person(
     name: Optional[str] = Query(
         None, 
@@ -95,9 +109,12 @@ def show_person(
     return {name: age}
 
 # Validaciones: Path Parameters
+persons = [1,2,3,4,5]
+
 @app.get(
     path="/person/detail/{person_id}",
-    status_code=status.HTTP_200_OK)
+    status_code=status.HTTP_200_OK,
+    tags=["Persons"])
 def show_person(
     person_id: int = Path(
         ..., 
@@ -106,12 +123,18 @@ def show_person(
         description="This is the id's Person"
         )
 ):
+    if person_id not in persons:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= "this person doesn't exist"
+        )
     return {person_id: "it exist"}
 
 # Validaciones: Request Body
 @app.put(
     path="/person/{person_id}",
-    status_code=status.HTTP_202_ACCEPTED)
+    status_code=status.HTTP_202_ACCEPTED,
+    tags=["Persons"])
 def update_peson(
     person_id: int = Path(
         ...,
@@ -126,7 +149,57 @@ def update_peson(
     results.update(location.dict())
     return person
 
-# Validaciones: Models
+# forms
 
+@app.post(
+    path="/login",
+    response_model= LoginOut,
+    status_code= status.HTTP_200_OK,
+    tags=["Persons"]
+)
+def login(
+    username: str = Form(...), 
+    password: str = Form(...)
+    ):
+    return LoginOut(username=username)
 
+# Cookies and headers
 
+@app.post(
+    path="/contact",
+    tags= ["Contact"]
+)
+def contact(
+    firts_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=2
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=2
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+            ...,
+            min_length=20
+        ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+    ):
+    return user_agent
+    
+# Files
+@app.post(
+    path="/upload-file",
+    tags=["Files"]
+    )
+def upload_file(
+    image: UploadFile = File(...)
+    ):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size (kb)": round(len(image.file.read())/1024, ndigits=2),
+    }
